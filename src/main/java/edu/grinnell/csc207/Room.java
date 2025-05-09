0985cba9ec009e29e8184d2e993d6954f68391f4
liyutong721002 <liyutong721002@gmail.com>
2025-05-09 01:35:39 -0500

    update 4 commands of room.

--- src/main/java/edu/grinnell/csc207/Room.java
+++ src/main/java/edu/grinnell/csc207/Room.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class Room {
 
     private List<Item> items;
+    private List<String> objects;
+    private Map<String, Room> connectedrooms;
     private String name;
     private Item wonderTree;
 
@@ -25,10 +27,61 @@ public class Room {
         this.wonderTree = tree;
     }
 
-    //Wait////wait in the room for one turn
-    //Go<direction> // go in the given cardinal direction, e.g., north or south
-    //Talk to<object> // talk to the given object found in the room
-    //Pick up<item> // pick up the given item found in the room
+    /**
+     * wait in the room for one turn
+     */
+    public void Wait() {
+        System.out.println("Time passes");
+    }
+
+    /**
+     * go in the given cardinal direction
+     *
+     * @param Direction a string that contains the direction that the player
+     * wants to go
+     */
+    public void Go(String Direction) {
+        Room next = connectedrooms.get(Direction);
+        if (next == null) {
+            System.out.println("The wind whispers that the path to the " + Direction + " does not yet exist...");
+        } else {
+            items = next.items;
+            connectedrooms = next.connectedrooms;
+            name = next.name;
+            wonderTree = next.wonderTree;
+            System.out.println("A gust of wind lifts you gently and places you at the edge of the " + name + ".");
+        }
+
+    }
+
+    /**
+     * talk to the given object found in the room
+     *
+     * @param object a String of the object that the player is trying talking to
+     */
+    public void TalkTo(String object) {
+        if (objects.contains(object.toLowerCase())) {
+            System.out.println("You try talking to the " + object + ", but it says nothing.");
+        } else {
+            System.out.println("You didn't see " + object);
+        }
+
+    }
+
+    /**
+     * pick up the given item found in the room
+     * @param item a string is the item that we want to pick up
+     */
+    public void PickUp(String item) {
+        if (items.contains(item.toLowerCase())) {
+            items.remove(item.toLowerCase());
+            //inventory.add(item.toLowerCase()); 
+            System.out.println("You picked up the " + item + ".");
+        } else {
+            System.out.println("You didn't see the " + item + ".");
+        }
+    }
+
     /**
      * Use the given item found in the player's inventory
      *
@@ -42,7 +95,7 @@ public class Room {
 
     }// attack the given object found in the room
 
-    public void Look at(Item item) {
+    public void Lookat(Item item) {
 
     }// look at the given object found in the room
 
