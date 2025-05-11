2fad678df6b6f0089e9af9732503b81ff682c651
tangyixu <tangyixu@grinnell.edu>
2025-05-11 12:42:18 -0500

    add intro

--- src/main/java/edu/grinnell/csc207/TropicalKingkom.java
+++ src/main/java/edu/grinnell/csc207/TropicalKingkom.java
@@ -11,16 +11,24 @@ import java.util.HashMap;
  *
  * @author yutong
  */
-public class TropicalKingkom extends Room{
+public class TropicalKingkom extends Room {
+
     private boolean guavaTaken;
-    
-    private String intro;
+
+    private String intro = "As soon as you open the door, a wave of humidity washes over you. "
+            + "You've arrived at the **Tropical Kingdom**! "
+            + "The calls of birds echo through the forest, "
+            + "and you spot venomous snakes flicking their "
+            + "tongues not far away. Vines sprawl in every direction. "
+            + "You really love guavas, and you remember that the tree "
+            + "they grow on is beautiful—like a rainbow—and is said to "
+            + "drop mysterious gifts. You're considering attacking the **Rainbow Tree**!";
 
     public TropicalKingkom() {
         super("DeathDessert", new ArrayList<>(), null, new HashMap<>(), null);
-        
+
         guavaTaken = false;
-        
+
         //TODO
         intro = "";
 
@@ -31,7 +39,7 @@ public class TropicalKingkom extends Room{
         this.addItem(guava);
         this.setWonderTree(tree);
         this.setGift(guava);
-        
+
         this.addConnectedRoom("South", new Tianya());
     }
 }
