d50d2fd017ca10c68777a8b9aff87de485d5fbdc
tangyixu <tangyixu@grinnell.edu>
2025-05-11 12:19:51 -0500

    update arctic zone

--- src/main/java/edu/grinnell/csc207/ArcticZone.java
+++ src/main/java/edu/grinnell/csc207/ArcticZone.java
@@ -6,6 +6,13 @@ package edu.grinnell.csc207;
  */
 public class ArcticZone extends Room {
 
-    private String introduction = 
+    private String introduction = "The biting cold wind lashes against your face. "
+            + "You've entered the Arctic Zone. "
+            + "Looking around, you see many penguins and glaciers. "
+            + "In the distance, something is shimmering. "
+            + "You recall an ancient legend that speaks of a giant Arctic Snow Lotus "
+            + "at the peak of the northern lands. "
+            + "It is said to bear mysterious fruit—fruit that grants magical powers."
+            + "You are thinking of attacking [Lotus].";
 
 }
