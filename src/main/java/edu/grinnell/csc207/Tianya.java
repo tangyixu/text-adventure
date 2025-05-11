7c569e051793db43b70c8aa625771d04e168de1a
tangyixu <tangyixu@grinnell.edu>
2025-05-11 12:32:11 -0500

    add intro

--- src/main/java/edu/grinnell/csc207/Tianya.java
+++ src/main/java/edu/grinnell/csc207/Tianya.java
@@ -11,8 +11,21 @@ import java.util.HashMap;
  *
  * @author yutong
  */
-public class Tianya extends Room{
-    
+public class Tianya extends Room {
+
+    private String intro = "You’ve entered a grand temple with four towering doors, "
+            + "each aligned with one of the cardinal directions—North, South, East, and West. "
+            + "Every door radiates a distinct aura: one blazes with the "
+            + "intensity of scorching sunlight, another glistens as though sealed in ice, "
+            + "a third echoes faintly with the rhythmic crashing of waves, "
+            + "and the last carries the thick, humid scent of a tropical rainforest drenched in rain."
+            + "You remember an old legend: "
+            + "if you collect the four Sacred Trees—each hidden within the elemental palaces "
+            + "beyond these doors—you will be granted one chance at rebirth… "
+            + "a chance to return and retake your CSC 207 exam, which you didn’t do so well on this time."
+            + "Driven by desperation and determination, you take a deep breath and prepare to begin your journey.\n"
+            + "Which direction will you choose first [North, South, East, West]?";
+
     public Tianya() {
         super("Moana", new ArrayList<>(), null, new HashMap<>());
 
@@ -22,8 +35,8 @@ public class Tianya extends Room{
         this.addItem(tree);
         this.addItem(coconut);
         this.setWonderTree(tree);
-        
+
         this.addConnectedRoom("West", new Tianya());
     }
-    
+
 }
